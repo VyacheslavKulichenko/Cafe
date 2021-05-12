4cf0fa2711389159c8dc2c1b1cf0538e8287cfdf
Vyacheslav Kulychenko <vyacheslav1527@gmail.com>
2021-05-12 12:59:00 +0300

    Initial commit

---
+++ app/src/main/java/com/kulichenko/cafe/OrderDetailActivity.java
@@ -0,0 +1,30 @@
+package com.kulichenko.cafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import android.os.Bundle;
+
+public class OrderDetailActivity extends AppCompatActivity {
+
+    private TextView textViewOrder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_detail);
+        textViewOrder = findViewById(R.id.textViewOrder);
+        Intent intent = getIntent();
+        if (intent.hasExtra("order")) {
+            String order = intent.getStringExtra("order");
+            textViewOrder.setText(order);
+        } else {
+            Intent backToLogin = new Intent(this, LoginActivity.class);
+            startActivity(backToLogin);
+        }
+    }
+}
